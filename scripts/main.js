83bf8b622ce47e4c707e0a177403deaa4f5ff9b8
arjunbharti <arjunbharti774@gmail.com>
2022-02-09 12:11:49 +0530

    chore: Added demo for modal

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,28 @@ copyBtnList.forEach(copyBtn => {
     navigator.clipboard.writeText(value);
   });
 });
+
+
+//modal popup
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+const modal = document.getElementById("modal");
+const modalBtn = document.getElementById("modalBtn");
+const closeModal = document.getElementById("closeModal");
+
+modalBtn.onclick = function(){
+  modal.style.display = "block";
+}
+
+closeModal.onclick = function(){
+  modal.style.display = "none";
+}
+
+window.onclick = function(event){
+  if(event.target == modal){
+    modal.style.display = "none";
+  }
+}
